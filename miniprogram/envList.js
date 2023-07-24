08c7990d204503e197359868f004f18d015f649a
zhupengliang <pengliang.zhu@clouddeep.cn>
2023-07-24 10:58:00 +0800

    feat: 配置云开发环境

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,4 +1,10 @@
-const envList = [];
+const envList = [
+  {
+    envId: 'cloud1-8ggb0v441269ef28',
+    alias: 'cloud1',
+    icon: 'https://example.com/icon1.png'
+  },
+];
 const isMac = false;
 module.exports = {
   envList,
