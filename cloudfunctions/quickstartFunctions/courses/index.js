75e2fb27bfc2e3c6e8003cf3347024f3137d5d67
zhupengliang <pengliang.zhu@clouddeep.cn>
2023-09-07 10:32:55 +0800

    feat: 支持超长和超多数据管理

--- cloudfunctions/quickstartFunctions/courses/index.js
+++ cloudfunctions/quickstartFunctions/courses/index.js
@@ -4,6 +4,7 @@ cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 });
 const db = cloud.database();
+const MAX_LIMIT = 100
 
 // 班级课程
 exports.main = async (event, context) => {
@@ -26,9 +27,28 @@ exports.main = async (event, context) => {
           msg: '缺少参数classId。',
         };
       }
-      return await courseDB.where({
+      // 先取出集合记录总数
+      const countResult = await courseDB.where({
         classId
-      }).orderBy("createdAt", "asc").get();
+      }).count()
+      const total = countResult.total
+      // 计算需分几次取
+      const batchTimes = Math.ceil(total / 100)
+      // 承载所有读操作的 promise 的数组
+      const tasks = []
+      for (let i = 0; i < batchTimes; i++) {
+        const promise = courseDB.where({
+          classId
+        }).skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+        tasks.push(promise)
+      }
+      // 等待所有
+      return (await Promise.all(tasks)).reduce((acc, cur) => {
+        return {
+          data: acc.data.concat(cur.data),
+          errMsg: acc.errMsg,
+        }
+      })
     case 'create': {
       // 插入
       const {
--- cloudfunctions/quickstartFunctions/timetables/index.js
+++ cloudfunctions/quickstartFunctions/timetables/index.js
@@ -4,6 +4,7 @@ cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 });
 const db = cloud.database();
+const MAX_LIMIT = 100
 
 // 作息时间
 exports.main = async (event, context) => {
@@ -20,9 +21,31 @@ exports.main = async (event, context) => {
       const {
         classId
       } = payload;
-      return await timetableDB.where({
+      // 先取出集合记录总数
+      const countResult = await timetableDB.where({
         classId
-      }).get();
+      }).count()
+      const total = countResult.total
+      // 计算需分几次取
+      const batchTimes = Math.ceil(total / 100)
+      // 承载所有读操作的 promise 的数组
+      const tasks = []
+      for (let i = 0; i < batchTimes; i++) {
+        const promise = timetableDB.where({
+          classId
+        }).skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+        tasks.push(promise)
+      }
+      // 等待所有
+      return (await Promise.all(tasks)).reduce((acc, cur) => {
+        return {
+          data: acc.data.concat(cur.data),
+          errMsg: acc.errMsg,
+        }
+      })
+      // return await timetableDB.where({
+      //   classId
+      // }).get();
     case 'upsert': {
       // 插入或更新
       const {
